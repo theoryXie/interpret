e43e1d2237ad8fe74b09c01fd2e1b9bf1936c893
Jasonix <ccjjxx99@qq.com>
2019-09-30 11:12:16 +0800

    增加生成SLR分析表的接口

--- interpret/src/main/java/whu/se/interpret/service/impl/ParserImpl.java
+++ interpret/src/main/java/whu/se/interpret/service/impl/ParserImpl.java
@@ -2,8 +2,8 @@ package whu.se.interpret.service.impl;
 
 import whu.se.interpret.po.Family;
 import whu.se.interpret.po.Node;
+import whu.se.interpret.po.SLRTable;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,26 +17,28 @@ import java.util.HashSet;
 public interface ParserImpl {
 
     //初始化，文法数据结构化，求出first，follow集
-    public void init(String grammarFileName) throws IOException;
+    void init(String grammarFileName) throws IOException;
 
     //读取文法文件，创建文法的数据结构
-    public ArrayList<Node> getGrammar(String filename) throws IOException;
+    ArrayList<Node> getGrammar(String filename) throws IOException;
 
 
     //获得first集
-    public HashMap<String, HashSet<String>> getAllFirst();
+    HashMap<String, HashSet<String>> getAllFirst();
 
 
     //获得follow集
-    public HashMap<String, HashSet<String>> getAllFollow();
+    HashMap<String, HashSet<String>> getAllFollow();
 
     //分析文法单元是终结符还是非终结符
     // true -- 终结符
     // false -- 非终结符
-    public boolean isTerm(String s);
+    boolean isTerm(String s);
 
-    //通过文法的数据结构生成I0项目集规范簇(Family)
-    public Family generateFamily(ArrayList<Node> grammer);
+    //通过文法的数据结构生成I项目集规范族(Family)
+    Family generateFamily(ArrayList<Node> grammar);
 
+    //通过项目集规范族生成SLR(1)分析表
+    SLRTable generateSLRTable(Family family);
 
 }
