dcd84e018c1a620bb034a66c42fa79a295a2a6c9
whuxsy <827041735@qq.com>
2019-10-03 17:44:20 +0800

    follow集更正

--- interpret/src/test/java/whu/se/interpret/implTest/ParserImplTest.java
+++ interpret/src/test/java/whu/se/interpret/implTest/ParserImplTest.java
@@ -32,19 +32,20 @@ public class ParserImplTest extends InterpretApplicationTests {
 
     @Test
     public void testInit() throws IOException {
-        parserImpl.init("grammar");
+        //parserImpl.init("grammar");
+        parserImpl.init("grammar.txt");
         HashMap<String, HashSet<String>> firstSet = parserImpl.getAllFirst();
         HashMap<String, HashSet<String>> followSet = parserImpl.getAllFollow();
-        Iterator iter = firstSet.entrySet().iterator();
-        while (iter.hasNext()) {
-            Map.Entry entry = (Map.Entry) iter.next();
-            System.out.printf("%-20s:%s\n",entry.getKey(),entry.getValue());
-        }
-//        Iterator iter = followSet.entrySet().iterator();
+//        Iterator iter = firstSet.entrySet().iterator();
 //        while (iter.hasNext()) {
 //            Map.Entry entry = (Map.Entry) iter.next();
 //            System.out.printf("%-20s:%s\n",entry.getKey(),entry.getValue());
 //        }
+        Iterator iter = followSet.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            System.out.printf("%-20s:%s\n",entry.getKey(),entry.getValue());
+        }
     }
 
 /** @Author: zfq
