6e7f5a13d2eed9d217c0b10b2d899f7c8e975278
Jasonix <ccjjxx99@qq.com>
2019-10-24 16:25:20 +0800

    解决数组的文法问题

---
+++ interpret/src/main/java/whu/se/interpret/po/symbol/Array.java
@@ -0,0 +1,17 @@
+package whu.se.interpret.po.symbol;
+
+/**
+ * @author ：Chang Jiaxin
+ * @date ：Created in 2019/10/24 16:23
+ * @description ： 数组
+ */
+public class Array extends NoTerminal {
+
+    /**
+     * @param name
+     */
+    public Array(String name) {
+        super(name);
+        // TODO Auto-generated constructor stub
+    }
+}
